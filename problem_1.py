b037313dbdf07d08728b73fe7c2528af377f6d3c
rhitabrat <pokharelrhitabrat@gmail.com>
2019-01-26 01:15:56 +0530

    edited problem_1

--- problem_1.py
+++ problem_1.py
@@ -2,31 +2,20 @@
 All students need to write a number on board from a particular range. They have to find a
 solution to find closest pair from numbers written.
 """
-
-l = []
-a = []
-b = []
 print("Enter the numbers separated by a space:")
 arr = input()
 l = list(map(int, arr.split(' ')))
 
-def closestPair(ls):
-    ls.sort() 
-    a = [ls[i + 1] - ls[i] for i in range(len(ls)-1)]
-    print(a)
-        
-    b = sorted(a)
-    print("B: " + str(b))
 
-    var = b[0]
-    idx = a.index(var)
-    # print(ind)
-    print("Closest numbers are " + str(ls[idx]) + " and " + str(ls[idx+1]) + " with a difference of " + str(var))
-    
-    j=0
-    while (a[j] == b[0]):
-        j =j+1
-        print(j)
-        print("Biatch")
-        
+def closestPair(ls):
+    ls.sort()
+    a = [ls[i + 1] - ls[i] for i in range(len(ls)-1)]
+
+    b = a.index(min(a))
+    idx = [index for index, value in enumerate(a) if value == b]
+
+    print("The closest pairs are:")
+    for i in idx:
+        print("("+str(ls[i])+", "+str(ls[i+1])+")")
+
 closestPair(l)
